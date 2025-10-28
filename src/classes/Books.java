39c09c9fb2582b2f8bca86593d2a21b71a7af879
Bless <tetaangelbless014@gmail.com>
2025-10-28 20:21:31 +0200

    Initialized constructors, getters and setters

--- src/classes/Books.java
+++ src/classes/Books.java
@@ -6,4 +6,42 @@ public class Books {
     private String author;
     private boolean isBorrowed;
     public Books(){}
+    public Books(int id, String title, String author,boolean isBorrowed){
+        this.id=id;
+        this.title=title;
+        this.author=author;
+        this.isBorrowed=false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
 }
