dcb5467000e4021c47cbc77ddcf57ac0da7c4b0e
Bless <tetaangelbless014@gmail.com>
2025-10-31 09:49:09 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import classes.Books;
 import classes.Library;
+import classes.Members;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Books book =
-        Library liby = new Library();
+        Books book = new Books();
+        Members member =new Members();
+        Library library = new Library();
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name= sc.nextLine();
+        member.setName(name);
+
+
 
     }
 }
